18d9144c35ea3625e0b29a4ad10e3b6ae876fdb2
S05p <121417498+S05p@users.noreply.github.com>
2024-01-25 14:18:30 +0900

    Update settings.py
    asdf

--- HealthLife/settings.py
+++ HealthLife/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^f)(xt(n$j4i=-4a8y^*f(5tb=f2w@5^-t$4_^lgksrx&14zlt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
